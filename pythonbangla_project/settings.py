50864850ed80f29a4bff0693c1ec7cd7bfd1f75f
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-05-06 18:25:14 +0800

    PostgreSQL connection done

--- pythonbangla_project/settings.py
+++ pythonbangla_project/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'pythonbangla_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "pythonbangla",
+        "USER": "mahmud",
+        "PASSWORD": "12345",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
