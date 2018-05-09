3c55dfc184afe80aa259df71b32c3a461851d4a1
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-05-09 12:31:56 +0800

    static and template moved from main_app to project level

--- pythonbangla_project/settings.py
+++ pythonbangla_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pythonbangla_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
